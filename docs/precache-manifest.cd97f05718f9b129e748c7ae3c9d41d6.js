d628a3583a270edb770ba473cba644fdd8344aa5
Esteban <estebanacanteros@gmail.com>
2020-08-22 14:33:57 -0300

    docs added

---
+++ docs/precache-manifest.cd97f05718f9b129e748c7ae3c9d41d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee8f695f288d7dae5d8357e139b18dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21bc0a57c89322510aeb",
+    "url": "/static/css/main.33c6be70.chunk.css"
+  },
+  {
+    "revision": "fb6fbd8ae526bc3997ad",
+    "url": "/static/js/2.7de57cdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7de57cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21bc0a57c89322510aeb",
+    "url": "/static/js/main.303f44fd.chunk.js"
+  },
+  {
+    "revision": "47835e80aaf3cd034a37",
+    "url": "/static/js/runtime-main.947fe9e4.js"
+  }
+]);
